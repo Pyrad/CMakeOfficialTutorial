7a895f3212aba8ad87b458495c4b606d00c0bc94
pyrad(asus) <cailong89@163.com>
2022-06-08 22:28:53 +0800

    How to defined macro in CMakeLists.txt
    Define some macros by CMake, and they can be used in source code.

--- Step5/MathFunctions/mysqrt.cxx
+++ Step5/MathFunctions/mysqrt.cxx
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <cmath>
+
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -8,8 +10,14 @@ double mysqrt(double x)
   if (x <= 0) {
     return 0;
   }
-
+  
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = exp(log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   double result = x;
+#endif
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
